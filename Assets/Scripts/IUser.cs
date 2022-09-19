8fa2a39b912967027ecdb511ec56c49613b6e64d
parkinyeop <parkinyeop@naver.com>
2022-09-19 14:07:52 +0900

    복구

--- Assets/Scripts/IUser.cs
+++ Assets/Scripts/IUser.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class IUser : MonoBehaviour
+interface IUser
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    Action onObjectUse { get; set; }    // Action타입의 프로퍼티
 }
